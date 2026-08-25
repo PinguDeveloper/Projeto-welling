010c2a8ee0ffb4a52261b0fc7f5de772f69c5554
PinguDeveloper <cleitingasques@gmail.com>
2026-08-25 07:21:04 -0300

    Mae do enzo

---
+++ src/assets/configurar-git.bat
@@ -0,0 +1,5 @@
+@echo off
+git config user.name "PinguDeveloper"
+git config user.email "cleitingasques@gmail.com"
+echo Git configurado com sucesso!
+pause
---
+++ src/components/AppInput.js
@@ -0,0 +1,12 @@
+import { TextInput } from "react-native";
+
+export default function AppInput({ label, error, ...props }) {
+    return (
+        <View>
+            {label && <Text>{label}</Text>}
+            <TextInput />
+            {error && <Text>{error}</Text>}
+        </View>
+    );
+
+}
